433293b3042c5d5f0e4a44136915b76ef6b75bcb
J.Henrique <jhenriqueribeirodesa@gmail.com>
2020-03-18 20:57:50 -0300

    add dotenv

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 
+require("dotenv/config");
+
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "public")));
